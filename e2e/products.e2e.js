3c7e46983801e6ec7a690b33e18fea84906c3f04
Robinson De La Cruz <rdelacruzsiado@gmail.com>
2023-04-05 10:22:55 -0500

    feat: test get one and post products

--- e2e/products.e2e.js
+++ e2e/products.e2e.js
@@ -38,6 +38,61 @@ describe('Test for products ', () => {
       expect(response.statusCode).toEqual(200);
       expect(response.body.length).toEqual(2);
     });
+
+    test('should return one product', async () => {
+      const product = await models.Product.findByPk(1);
+      const response = await api.get(`${endpoint}/1`);
+
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.name).toEqual(product.name);
+    });
+
+    test('return 404 when it does not exist a product with that ID', async () => {
+      const response = await api.get(`${endpoint}/100`);
+
+      expect(response.statusCode).toEqual(404);
+    });
+  });
+
+  describe('POST /products', () => {
+    test('should create a product', async () => {
+      const inputData = {
+        name: 'New Product',
+        price: 10000,
+        description: 'A description',
+        categoryId: 1,
+        image: 'https://api.lorem.space/image/game?w=150&h=220',
+      };
+      const response = await api.post(endpoint).send(inputData);
+      const productCreated = await models.Product.findByPk(response.body.id);
+
+      expect(response.statusCode).toEqual(201);
+      expect(productCreated.name).toEqual(inputData.name);
+    });
+
+    test('return 404 when it does not exist the category asigned', async () => {
+      const inputData = {
+        name: 'New Product',
+        price: 10000,
+        description: 'A description',
+        categoryId: 100,
+        image: 'https://api.lorem.space/image/game?w=150&h=220',
+      };
+      const response = await api.post(endpoint).send(inputData);
+
+      expect(response.statusCode).toEqual(404);
+    });
+
+    test('return error when the properties required are not send', async () => {
+      const inputData = {
+        name: 'New Product',
+        categoryId: 100,
+        image: 'https://api.lorem.space/image/game?w=150&h=220',
+      };
+      const response = await api.post(endpoint).send(inputData);
+
+      expect(response.statusCode).toEqual(400);
+    });
   });
 
   afterAll(async () => {
